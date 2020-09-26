9dbcbe0157c4a715310dd3545c7a583300040937
KoushikSahu <koushiksahu68@gmail.com>
2020-09-26 13:40:40 +0530

    changed cpp template, vimrc; deleted vs-code-cpp json

--- templates/cpp-template.cpp
+++ templates/cpp-template.cpp
@@ -10,11 +10,20 @@ typedef vector<vll> vvll;
 typedef pair<int, int> ipair;
 typedef pair<ll, ll> llpair;
 
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep; for (const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename T, size_t size> ostream& operator<<(ostream &os, const array<T, size> &arr) { os << '{'; string sep; for (const auto &x : arr) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
 
 
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL), cout.tie(NULL);
-
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    
+    return 0;
 }
