9095647f3160124d9f429ee11cb4ce7189fe9ddb
Koushik Sahu <koushiksahu68@gmail.com>
2021-04-13 13:27:54 +0530

    added modulo to binary exponentiation

--- algorithms/binary_exponentiation.cpp
+++ algorithms/binary_exponentiation.cpp
@@ -3,18 +3,23 @@ using namespace std;
 
 //-------------------------<copy>-----------------------
 template <typename T>
-T pow_binexpo(T x, int y){
+T pow_binexpo(T x, ll y, ll m){
+    x %= m;
     T ans = 1; 
     while(y){
-        if(y%2) ans *= x;
+        if(y%2){
+            ans *= x;
+            ans %= m;
+        }
         x *= x;
+        x %= m;
         y /= 2;
     }
     return ans;
 }
 
 template <typename T>
-T pow_matexpo(T x, int y){
+T pow_matexpo(T x, ll y){
     assert(x.n == x.m);
     T ans = T(x.n, x.m, true);
     while(y){
